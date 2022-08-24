4c516966d2336a5e83ac49588af962230854c0f0
Surya Darma Putra <surya.igede@gmail.com>
2022-08-24 09:07:57 +0800

    chore: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  stories: [
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-tailwind-dark-mode',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['postcss-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './.storybook/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: colors.slate,
+        primary: colors.indigo,
+        success: colors.emerald,
+        danger: colors.rose,
+        warning: colors.amber,
+        info: colors.sky,
+      },
+    },
   },
   plugins: [],
 };
