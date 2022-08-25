5adec36324a3c5d711ecf4719eaa1c54a1818a15
Surya Darma Putra <surya.igede@gmail.com>
2022-08-25 15:06:55 +0800

    chore: script to prepare storybook demo

---
+++ scripts/prepare-demo.sh
@@ -0,0 +1,5 @@
+cp -v storybook-static/*.js out/
+cp -v storybook-static/*.json out/
+cp -v storybook-static/*.txt out/
+cp -v storybook-static/iframe.html out/
+cp -v storybook-static/index.html out/storybook.html
