1ed810f516e384a49f9596deb27fb4504e880360
Surya Darma Putra <surya.igede@gmail.com>
2022-08-25 15:14:18 +0800

    chore: fix storybook demo directory

--- scripts/prepare-demo.sh
+++ scripts/prepare-demo.sh
@@ -2,4 +2,5 @@ cp -v storybook-static/*.js out/
 cp -v storybook-static/*.json out/
 cp -v storybook-static/*.txt out/
 cp -v storybook-static/iframe.html out/
-cp -v storybook-static/index.html out/storybook.html
+mkdir out/storybook
+cp -v storybook-static/index.html out/storybook/
